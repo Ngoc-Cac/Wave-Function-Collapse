2f12c30bbab03e7537b723ac8538dc8fe600b7fb
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-02-03 16:10:14 +0700

    Update utils.py
    refactored code

--- src/wfc/utils.py
+++ src/wfc/utils.py
@@ -7,6 +7,10 @@ import matplotlib.figure
 
 from scipy.ndimage import rotate
 
+from wfc.cell_image import Direction, TileImage
+
+from typing import Iterable
+
 
 def _augment_by_rotation(patterns: list[np.ndarray]) -> list[np.ndarray]:
     rotated_patterns = []
@@ -24,9 +28,10 @@ def _augment_by_rotation(patterns: list[np.ndarray]) -> list[np.ndarray]:
             rotated_patterns.append(twensev)
     return rotated_patterns
 
-def show_tiles(tiles) -> tuple[matplotlib.figure.Figure, np.ndarray[plt.Axes]]:
-    rows = int(np.sqrt(len(tiles)))
-    cols = int(np.ceil(len(tiles) / rows))
+def show_tiles(images: Iterable[np.ndarray])\
+    -> tuple[matplotlib.figure.Figure, np.ndarray[plt.Axes]]:
+    rows = int(np.sqrt(len(images)))
+    cols = int(np.ceil(len(images) / rows))
     fig, ax_arr = plt.subplots(rows, cols)
     if rows == 1:
         temp = np.ndarray((1, cols), dtype=object)
@@ -35,11 +40,12 @@ def show_tiles(tiles) -> tuple[matplotlib.figure.Figure, np.ndarray[plt.Axes]]:
 
     for row in ax_arr:
         for ax in row: ax.axis('off')
-    for i, tile in enumerate(tiles):
-        ax_arr[(i - i % cols) // cols, i % cols].imshow(tile)
+    for i, image in enumerate(images):
+        ax_arr[(i - i % cols) // cols, i % cols].imshow(image)
     return fig, ax_arr
 
-def load_patterns(directory: str, rotate: bool = True) -> list[np.ndarray]:
+def load_patterns(directory: str, rotate: bool = True)\
+    -> list[np.ndarray]:
     """
     Load a list of numpy.ndarray representing the tiles and its rotated version.
     
@@ -58,15 +64,45 @@ def load_patterns(directory: str, rotate: bool = True) -> list[np.ndarray]:
 
 def generate_patterns(image_filepath: str,
                       n_pixels: int = 3,
-                      rotate: bool = False) -> list[np.ndarray]:
+                      rotate: bool = False)\
+    -> list[TileImage]:
     image = cv2.cvtColor(cv2.imread(image_filepath), cv2.COLOR_BGR2RGB)
 
-    patterns = []
+    unique_patterns = {}
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             row_indices = [[(i + offset) % image.shape[0]] for offset in range(n_pixels)]
             col_indices = [(j + offset) % image.shape[1] for offset in range(n_pixels)]
-            patterns.append(image[row_indices, col_indices])
+            
+            sub_image = image[row_indices, col_indices]
+            temp_key = tuple(sub_image.flatten())
+            if temp_key in unique_patterns: unique_patterns[temp_key][1] += 1
+            else: unique_patterns[temp_key] = [sub_image, 1]
 
-    if rotate: patterns.extend(_augment_by_rotation(patterns))
-    return patterns
+    if rotate: 
+        for rotated_pattern in _augment_by_rotation([value[0] for value in unique_patterns.values()]):
+            temp_key = tuple(rotated_pattern.flatten())
+            unique_patterns[temp_key] = [1, rotated_pattern]
+    return [_OverlappingModel_TileImage(*value) for value in unique_patterns.values()]
+
+
+class _OverlappingModel_TileImage(TileImage):
+    @TileImage.image.getter
+    def image(self) -> np.ndarray:
+        image = np.ndarray((1, 1, 3), dtype='uint8')
+        image[0, 0] = self._pattern[0, 0]
+        return image
+    
+    def is_adjacent_to(self, tile: '_OverlappingModel_TileImage', direction: Direction) -> bool:
+        match direction:
+            case Direction.UP:
+                result = (self._pattern[1:] == tile._pattern[0:-1]).all()
+            case Direction.DOWN:
+                result = (self._pattern[0:-1] == tile._pattern[1:]).all()
+            case Direction.LEFT:
+                result = (self._pattern[:, 1:] == tile._pattern[:, 0:-1]).all()
+            case Direction.RIGHT:
+                result = (self._pattern[:, 0:-1] == tile._pattern[:, 1:]).all()
+            case _:
+                raise ValueError("Weird direction passed")
+        return result
